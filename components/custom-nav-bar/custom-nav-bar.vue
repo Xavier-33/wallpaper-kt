5ddfa7bd0736bbc552629f6b8d0bcea638196664
xavier_native <1832941391@qq.com>
2025-10-27 00:37:58 +0800

    create custom navigator bar

--- components/custom-nav-bar/custom-nav-bar.vue
+++ components/custom-nav-bar/custom-nav-bar.vue
@@ -1,13 +1,74 @@
 <template>
-	<view class="">
-
+	<view class="layout">
+		<view class="navbar">
+			<view class="status-bar" :style="{height:statusBarHeight+'px'}"></view>
+			<view class="title-bar" :style="{height:titleBarHeight+'px'}">
+				<view><text class="title">标题</text></view>
+				<view class="search">
+					<uni-icons class="search-icon" type="search" color="#888" size="18"></uni-icons>
+					<text class="search-text">搜索</text>
+				</view>
+			</view>
+		</view>
+		
+		<view class="fill" :style="{height:statusBarHeight+titleBarHeight+'px'}"></view>
 	</view>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+let SYSTEM_INFO = uni.getSystemInfoSync();
+let statusBarHeight = ref(SYSTEM_INFO.statusBarHeight);
+
+let {top, height} = uni.getMenuButtonBoundingClientRect();
+let titleBarHeight = ref(height + (top - statusBarHeight.value) * 2);
 
 </script>
 
 <style lang="scss" scoped>
-
+.layout {
+	.navbar {
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		z-index: 10;
+		background:
+			linear-gradient(to bottom, rgba(0,0,0,0), #fff 400rpx),
+			linear-gradient(to right, #beecd8 20%, #f4e2d8);
+		.status-bar {
+			// border: 1px solid red;
+		}
+		.title-bar {
+			height: 100rpx;
+			display: flex;
+			align-items: center;
+			padding: 0 30rpx;
+			// border: 1px solid green;
+			.title {
+				font-size: 22px;
+				font-weight: 700;
+				color: $text-font-color-1;
+			}
+			.search {
+				width: 220rpx;
+				height: 50rpx;
+				border-radius: 60rpx;
+				background: rgba(255, 255, 255, 0.4);
+				border: 1px solid #fff;
+				margin-left: 30rpx;
+				color: #999;
+				font-size: 28rpx;
+				display: flex;
+				align-items: center;
+				.search-icon {
+					margin-left: 5rpx;
+				}
+				.search-text {
+					padding-left: 10rpx;
+				}
+			}
+		}
+	}
+}
 </style>
